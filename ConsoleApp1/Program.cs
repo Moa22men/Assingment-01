09148ac51c6a85336b260777022d0777601a35ce
Moa22men <moamenabdelrahman22@gmail.com>
2025-06-22 12:30:46 +0300

    Q1 solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Moamen");
+            #region Q1
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+            #endregion
+
         }
     }
 }
