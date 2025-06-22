ba8f5a6880138df256ca87f48481c7e8a03ef350
Moa22men <moamenabdelrahman22@gmail.com>
2025-06-22 12:48:21 +0300

    Q3 solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,10 +12,18 @@ namespace ConsoleApp1
             #endregion
 
             #region Q2
-            string i = "7531_MMM";
-            int j = Convert.ToInt32(i);
-            Console.WriteLine(j);
+             string i = "7531_MMM";
+             int j = Convert.ToInt32(i);
+             Console.WriteLine(j);
             // the program will show FormatException :=> System.FormatException: 'Input string was not in a correct format.'
+            #endregion
+
+            #region Q3
+            float x = 5.5f;
+            float y = 1.6f;
+            float result = x - y;
+            Console.WriteLine(result);
+            // result = 3.9
             #endregion
         }
     }
